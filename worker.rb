2545a20631ee2ac44dd28707b71c66ff85297e85
nicklegr <addr.for+github@gmail.com>
2015-01-07 22:43:22 +0900

    sleep長めに

--- worker.rb
+++ worker.rb
@@ -18,8 +18,8 @@ class Watch
         # 不明なエラーのときも、落ちずに動き続ける
         puts "#{e} (#{e.class})"
         puts e.backtrace
-
-        sleep 10
+      ensure
+        sleep 3.hours
       end
     end
   end
